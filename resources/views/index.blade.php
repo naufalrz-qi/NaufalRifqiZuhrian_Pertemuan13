a7835832aa595ad66e15ba296d7c17da74c07265
naufalrz-qi <estarossa.nrz@gmail.com>
2023-10-10 10:56:09 +0800

    adminLTE

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index</title>
-</head>
-<body>
-    <h1>TEST</h1>
-</body>
-</html>
+@extends('template')
+@section('content')
+<!-- Default box -->
+<div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Title</h3>
+
+      <div class="card-tools">
+        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+          <i class="fas fa-minus"></i>
+        </button>
+        <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+          <i class="fas fa-times"></i>
+        </button>
+      </div>
+    </div>
+    <div class="card-body">
+      Start creating your amazing application!
+    </div>
+    <!-- /.card-body -->
+    <div class="card-footer">
+      Footer
+    </div>
+    <!-- /.card-footer-->
+  </div>
+  <!-- /.card -->
+
+@endsection
